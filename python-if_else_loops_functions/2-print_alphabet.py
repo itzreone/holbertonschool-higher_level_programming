6627ad4f3eb6ee1ebdb71de96f19a668dfa16442
itzreone <itzreone@gmail.com>
2024-05-17 18:46:16 +0400

    alph

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-letter = "a"
-output = ""
-while letter < "z":
+letter = 'a'
+output = ''
+while letter <= 'z':
     output += letter
     letter = chr(ord(letter) + 1)
-print(output)
+print("{}".format(output))
