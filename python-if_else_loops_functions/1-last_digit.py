0fc23d0d5876161a3d7225cb3188f9038ff52542
itzreone <itzreone@gmail.com>
2024-05-17 18:25:22 +0400

    Refactor if-else logic for positive or negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-last_digit_negative = last_digit - 10
+l_d = number % 10
+l_d_n = l_d - 10
 
 if number>0:
-    if last_digit > 5:
-        print("Last digit of", number, "is", last_digit, "and is greater than 5")
-    elif last_digit == 0:
-        print("Last digit of", number, "is", last_digit, "and is 0")
+    if l_d > 5:
+        print("Last digit of", number, "is", l_d, "and is greater than 5")
+    elif l_d == 0:
+        print("Last digit of", number, "is", l_d, "and is 0")
     else:
-        print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+        print("Last digit of", number, "is", l_d, "and is less than 6 and not 0")
 elif number == 0:
-        print("Last digit of", number, "is", last_digit, "and is 0")
+        print("Last digit of", number, "is", l_d, "and is 0")
 else:
-    if last_digit_negative == 0:
-        print("Last digit of", number, "is", last_digit_negative, "and is 0")
+    if l_d_n == 0:
+        print("Last digit of", number, "is", l_d_n, "and is 0")
     else:
-        print("Last digit of", number, "is", last_digit_negative, "and is less than 6 and not 0")
-        
+        print("Last digit of", number, "is", l_d_n, "and is less than 6 and not 0")
