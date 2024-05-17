e29ccb0c688cb6fae023d2f74c8d5baa6e983291
itzreone <itzreone@gmail.com>
2024-05-17 18:38:42 +0400

    Refactor if-else logic for positive or negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 l_d = number % 10
-l_d_n = l_d - 10
+if number >= 0:
+    l_d = number % 10
+else:
+    l_d = abs(number) % 10
+    l_d = -l_d
 str = "and is less than 6 and not 0"
 
-if number >= 0:
-    if l_d > 5:
-        print("Last digit of", number, "is", l_d, "and is greater than 5")
-    elif l_d == 0: 
-        print("Last digit of", number, "is", l_d, "and is 0")
-    else:
-        print("Last digit of", number, "is", l_d, str)
+
+if l_d > 5:
+    print("Last digit of", number, "is", l_d, "and is greater than 5")
+elif l_d == 0:
+    print("Last digit of", number, "is", l_d, "and is 0")
 else:
-        print("Last digit of", number, "is", l_d_n, str)
+    print("Last digit of", number, "is", l_d, str)
