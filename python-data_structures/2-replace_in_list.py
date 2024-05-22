83974e20a206e00193d0039be7c7e5b8211e8ddb
Ravan <itzreone@gmail.com>
2024-05-22 14:07:44 +0400

    upd

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,6 +5,5 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx > lenght:
         return my_list
-    else:
-        my_list[idx] = element
+    my_list[idx] = element
     return my_list
