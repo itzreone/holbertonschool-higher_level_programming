4a86ac01c93cc1d98e1472721ae08c61b12cf39d
Ravan <itzreone@gmail.com>
2024-05-22 13:57:49 +0400

    upd

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    in len = len(my_list) - 1
+    int len = len(my_list) - 1
     if idx < 0:
         return my_list
     if idx > len:
